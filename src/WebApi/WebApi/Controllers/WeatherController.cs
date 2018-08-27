1599a6fd39c6295615724e057c0cf57711106f0e
Jörg Weissbecker <joergweissbecker@web.de>
2018-08-27 21:55:09 +0200

    added initial project with sample data

---
+++ src/WebApi/WebApi/Controllers/WeatherController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("forecast")]
+        public ActionResult<IEnumerable<DayForecast>> Forecast(string city)
+        {
+            return new[] {new DayForecast(5, 5, 5), new DayForecast(1, 2, 4)};
+        }
+    }
+}
---
+++ src/WebApi/WebApi/Model/DayForecast.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.Model
+{
+    public class DayForecast
+    {
+        public DayForecast(decimal humidity, int temperature, decimal windSpeed)
+        {
+            Humidity = humidity;
+            Temperature = temperature;
+            WindSpeed = windSpeed;
+        }
+        public decimal Humidity { get; private set; }
+        public int Temperature { get; private set; }
+        public decimal WindSpeed { get; private set; }
+    }
+}
